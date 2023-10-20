63f74f240772ff91fdf25484df5d58033ce547b7
Danil <bloodshoww@mail.ru>
2023-10-20 10:50:32 +0300

    bugfix in sort

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import Style from './Cards.module.scss'
 import Index from "./index.jsx";
 import { AppContext } from "../../App.jsx";
+import {optionsItems} from "../../common/constants.js";
 
 
 
 const Cards = () => {
     const {items, fatsSort, sortType, fillersSort, onAddToCart} = React.useContext(AppContext)
-
+    console.log(sortType)
     return (
         <section className={Style.card}>
             <div className="container">
                 <div className={Style.cardWrapper}>
                     {
                         items
+                            .sort((a, b) => {
+                                switch (sortType) {
+                                    case 'price':
+                                        return a.price - b.price
+
+                                    case '-price':
+                                        return b.price - a.price
+
+                                    case 'popular':
+                                        return b.rating - a.rating;
+
+                                    default:
+                                        return 0;
+                                }
+                            })
                             .reduce((acc, item) => {
                                 if (item.fats == fatsSort || fatsSort == undefined) {
                                     acc.push(item)
@@ -26,7 +42,6 @@ const Cards = () => {
                                 }
                                 return acc
                             }, [])
-                            
                             .map(cart =>
                             <Index
                                 key={cart.id}
